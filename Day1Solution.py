3224d54a893dcf048b90847ee48515294e69020d
CodeDamnIt <94355357+CodeDamnIt@users.noreply.github.com>
2021-11-15 23:43:33 +0530

    Update Day1Solution.py

--- Day1Solution.py
+++ Day1Solution.py
@@ -3,3 +3,17 @@ santa=open("./Data/Day1Input.txt","r")
 text= santa.read()
 floor=text.count('(')-text.count(')')
 print(floor)
+santa.close()
+#Part 2
+negative=open("./Data/Day1Input.txt")
+basement = 0
+for i in negative:
+    for j in range(len(i)):
+        if (i[j] == "("):
+            basement+=1
+        else:
+            basement-=1
+        if (basement == -1):
+            print(j+1)
+            break
+negative.close()
