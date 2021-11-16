eb4645a5078be8db8ddf4f3579560a7cf322180c
CodeDamnIt <94355357+CodeDamnIt@users.noreply.github.com>
2021-11-16 09:20:01 +0530

    Update Day2Solutions.py

--- Day2Solutions.py
+++ Day2Solutions.py
@@ -1,8 +1,9 @@
-#Part1
+#Part1 and Part 2 Both
 cardboard=open("./Data/Day2input.txt","r")
 content=cardboard.read()
 f=content.splitlines()
 Surfarea=0
+Ribbon=0
 for words in f:
 	split=words.split('x')
 	l=int(split[0])
@@ -10,4 +11,7 @@ for words in f:
 	h=int(split[2])
 	area=2*(l*b+b*h+l*h) + min(l*b,b*h,h*l)
 	Surfarea+=area
-print(Surfarea)
+	Ribbon+=l*b*h+min(2*(l+b),2*(b+h),2*(l+h))
+print("Elves should order",Surfarea,"square feet of wrapping paper")
+print("Elves should order",Ribbon,"feet of ribbon")
+cardboard.close()
