992fa05d866b0851019e10aef8a99d0adcce4f6b
CodeDamnIt <94355357+CodeDamnIt@users.noreply.github.com>
2021-11-17 19:18:24 +0530

    Update Day3Solutions.py

--- Day3Solutions.py
+++ Day3Solutions.py
@@ -2,15 +2,15 @@
 santa=open("./Data/Day3input.txt")
 content=santa.read()
 gift=[0,0]
-grid= ['0,0']
+grid= [ ]
 for i in content:
-	if i == '^':
+	if i == "^":
 		gift= [gift[0],gift[1]+1]
 		grid.append(str(gift))
-	elif i == '<':
+	elif i == "<":
 		gift= [gift[0]-1,gift[1]]
 		grid.append(str(gift))
-	elif i == '>':
+	elif i == ">":
 		gift= [gift[0]+1,gift[1]]
 		grid.append(str(gift))
 	else:
@@ -18,4 +18,41 @@ for i in content:
 		grid.append(str(gift))
 houses=set(grid)
 print(len(houses))
+#Part2
+Claus = [0,0]
+Robo = [0,0]
+Grid = [ ]
+j = 0
+for d in content:
+    j+=1 
+    if d == ">":
+        if j % 2 == 0:
+            Claus=[Claus[0]+1,Claus[1]]
+            Grid.append(str(Claus))
+        else:
+            Robo= [Robo[0]+1,Robo[1]]
+            Grid.append(str(Robo))
+    elif d == "<": 
+        if j % 2 == 0:
+            Claus=[Claus[0]-1,Claus[1]]
+            Grid.append(str(Claus))
+        else:
+            Robo=[Robo[0]-1,Robo[1]]
+            Grid.append(str(Robo))
+    elif d == "^":
+        if j % 2 == 0:
+            Claus=[Claus[0],Claus[1]+1]
+            Grid.append(str(Claus))
+        else:
+            Robo=[Robo[0],Robo[1]+1]
+            Grid.append(str(Robo))
+    else:
+        if j % 2 == 0:
+            Claus=[Claus[0],Claus[1]-1]
+            Grid.append(str(Claus))  
+        else:
+            Robo=[Robo[0],Robo[1]-1]
+            Grid.append(str(Robo))
+santa.close()         
+print(len(set(Grid)))
 
